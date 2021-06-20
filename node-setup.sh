4a9bef9fe25f1fbb3bff66b79220a9f20cce1b4e
Gibah Joseph <gibahjoe@gmail.com>
2021-06-20 14:45:49 +0100

    updated

--- node-setup.sh
+++ node-setup.sh
@@ -4,3 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
+sudo rm -f /usr/bin/node
+sudo rm -f /usr/bin/npm
+sudo ln -s $(which node) /usr/bin/
+sudo ln -s $(which npm) /usr/bin/
